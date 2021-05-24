c98b3af13bd5e85746a6c5e47d5a5dfcb7afccc2
gokulmaxi <gokulakams@gmail.com>
2021-05-25 00:06:53 +0530

    nvim config added

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,89 @@
+return require('packer').startup(function()
+
+    -- Packer can manage itself as an optional plugin
+    use {
+        'wbthomason/packer.nvim',
+        opt = false
+    }
+    -- LSP config
+    use {'neovim/nvim-lspconfig'}
+    -- use { 'kabouzeid/nvim-lspinstall'}
+    use {'sbdchd/neoformat'}
+    -- Snippets
+    use {'hrsh7th/vim-vsnip'}
+    use {'rafamadriz/friendly-snippets'}
+    --    use {'cstrap/python-snippets'}
+    use{'nvim-lua/completion-nvim'}
+    --    use {'ylcnfrht/vscode-python-snippet-pack'}
+    --    use {'xabikos/vscode-javascript'}
+    --    use {'golang/vscode-go'}
+    --    use {'rust-lang/vscode-rust'}
+    -- use { 'honza/vim-snippets' }
+    -- use { 'SirVer/ultisnips' }
+    -- use { 'norcalli/snippets.nvim' }
+    -- use { 'nvim-telescope/telescope-snippets.nvim' }
+
+    -- Lua development
+    --    use {'tjdevries/nlua.nvim'}
+
+    -- Plugin development
+    --    use {'thinca/vim-themis'}
+    --    use {'tpope/vim-scriptease'}
+    --    use {'junegunn/vader.vim'}
+
+    --     -- Better syntax
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+    use{'norcalli/nvim-colorizer.lua'}
+
+    --     -- use {'nvim-treesitter/playground'}
+
+    --     -- Dashboard
+    --     use {'glepnir/dashboard-nvim'}
+
+    --     -- Status line
+    --     use {
+    --         'glepnir/galaxyline.nvim',
+    --         branch = 'main',
+    --         config = function() require 'statusline' end
+    --     }
+
+    --     -- Debugging
+    --     use {'puremourning/vimspector'}
+    --     use {'nvim-telescope/telescope-vimspector.nvim'}
+
+    --     -- Telescope fzf
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    }
+
+    --     -- Project
+    --    use {'nvim-telescope/telescope-project.nvim'}
+    --     use {'airblade/vim-rooter'}
+    --     -- use {'tpope/vim-projectionist'}
+    -- themes
+    use {'arcticicestudio/nord-vim'}
+    use {'sainnhe/gruvbox-material'}
+    --     -- Markdown
+    --   use {'npxbr/glow.nvim', run = ':GlowInstall'}
+    use {'mzlogin/vim-markdown-toc'}
+    use {
+        'iamcco/markdown-preview.nvim',
+        run = 'cd app && yarn install'
+    }
+    use {'godlygeek/tabular'}
+    use {'plasticboy/vim-markdown'}
+
+    --     -- Embed in browser
+    --     -- use {'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+    --     -- Neoterm
+    --     -- use {'kassio/neoterm' }
+
+    --     -- Better terminal
+    --     -- use {'nikvdp/neomux' }
+
+end)
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,72 @@
+
+
+local cmd = vim.cmd
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
+local indent = 4
+
+cmd 'syntax enable'
+cmd 'filetype plugin indent on'
+
+bo.shiftwidth = indent
+bo.tabstop = indent
+bo.softtabstop = indent
+o.termguicolors = true
+o.hidden = true
+o.ignorecase = true
+o.scrolloff = 4
+o.splitbelow = true
+o.splitright = true
+o.wildmode = 'list:longest'
+o.clipboard = 'unnamed,unnamedplus'
+o.timeoutlen = 500
+wo.number = true
+wo.relativenumber = false
+wo.scrolloff = 8
+wo.cursorline = true
+
+cmd [[
+set expandtab smarttab shiftround autoindent smartindent smartcase
+]]
+
+-- Highlight on yank
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+--TODO make auto format work
+-- Auto format
+-- vim.api.nvim_exec([[
+-- augroup auto_fmt
+--     autocmd!
+--     autocmd BufWritePre *.py,*.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+-- aug END
+-- ]], false)
+
+vim.api.nvim_exec([[
+augroup auto_spellcheck
+    autocmd!
+    autocmd BufNewFile,BufRead *.md setlocal spell
+    autocmd BufNewFile,BufRead *.org setfiletype markdown
+    autocmd BufNewFile,BufRead *.org setlocal spell
+    autocmd BufNewFile,BufRead *.html setlocal ts=2 sw=2 expandtab
+    autocmd BufNewFile,BufRead *.js setlocal ts=2 sw=2 expandtab
+    autocmd BufNewFile,BufRead *.json setlocal syntax=off expandtab
+augroup END
+]], false)
+
+vim.api.nvim_exec([[
+augroup auto_term
+    autocmd!
+    autocmd TermOpen * setlocal nonumber norelativenumber
+    autocmd TermOpen * startinsert
+augroup END
+]], false)
+
+vim.api.nvim_exec([[
+    fun! TrimWhitespace()
+        let l:save = winsaveview()
+        keeppatterns %s/\s\+$//e
+        call winrestview(l:save)
+    endfun
+    autocmd FileType go,rust,html,typescript,javascript,python autocmd BufWritePre <buffer> call TrimWhitespace()
+]], false)
+cmd 'colorscheme nord'
