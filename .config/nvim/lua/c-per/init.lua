58fc92cad11173980ae9dd86022ef8c681d832b2
gokulmaxi <gokulakams@gmail.com>
2021-06-13 12:15:27 +0530

    update c personel in nvim to c and cpp

--- .config/nvim/lua/c-per/init.lua
+++ .config/nvim/lua/c-per/init.lua
@@ -4,11 +4,18 @@ function cPer.startDebuging()
     -- vim.cmd [[call vimspector#Reset()]]
 --<CR> :!gcc -g % -o %< && %< <CR>
     vim.cmd [[:w ]]
+   if (vim.bo.filetype == "c") then
     vim.cmd [[:!gcc % -g -o %<]]
+    end
     vim.cmd [[:call vimspector#Launch()]]
 end
 function cPer.runCode()
         vim.cmd [[:w]]
+        if (vim.bo.filetype=="c") then
         vim.cmd [[:!gcc %  -o %< && %< ]]
+        end
+        if (vim.bo.filetype == "cpp") then
+        vim.cmd [[:!g++ %  -o %< && %< ]]
+        end
 end
         return cPer
