0be1925b9e2b036a0ff564ae51473b4ef3e065e9
gokul <gokulakams@gmail.com>
2021-10-17 11:07:47 +0530

    moved tex.vim to tex.lua

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,2 +1,18 @@
 require'lspconfig'.texlab.setup{}
-vim.g.diagnostic_enable_virtual_text = 0   
+vim.g.diagnostic_enable_virtual_text = 0
+vim.cmd [[
+function! AddItem()
+  let [end_lnum, end_col] = searchpairpos('\\begin{', '', '\\end{', 'nW')
+  if match(getline(end_lnum), '\(itemize\|enumerate\|description\)') != -1
+    return "\\item "
+  else
+    return ""
+  endif
+endfunction
+]]
+
+vim.cmd[[
+inoremap <expr><buffer> <CR> getline('.') =~ '\item $' ? '<c-w><c-w>'  : (col(".") < col("$") ? '<CR>' : '<CR>'.AddItem() )
+noremap <expr><buffer> o "o".AddItem()
+nnoremap <expr><buffer> O "O".AddItem()
+]]
