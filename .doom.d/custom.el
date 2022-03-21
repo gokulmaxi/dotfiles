16b4bbb6017c09f18b9d8a6b9bac6ba9a03dd465
gokul <gokulakams@gmail.com>
2022-03-21 23:28:52 +0530

    added jj for esc emacs

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -12,3 +12,7 @@
  )
 (setq  x-meta-keysym 'super
        x-super-keysym 'meta)
+;;Exit insert mode by pressing j and then j quickly
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -14,7 +14,8 @@
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
-
+(package! key-chord)
+  ; :recipe (:host github: "emacsorphanage/key-chord"))
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
